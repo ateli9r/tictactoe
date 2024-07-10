97e78f0c8bad7495d8ebebdf3d9e61526c080460
ateli9r <ateli9r@gmail.com>
2024-07-10 22:30:25 +0900

    #

--- src/main/java/egovframework/ateli9r/cmmn/config/CorsFilter.java
+++ src/main/java/egovframework/ateli9r/cmmn/config/CorsFilter.java
@@ -1,41 +1,46 @@
 package egovframework.ateli9r.cmmn.config;
 
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import jakarta.servlet.annotation.WebFilter;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {"/*"})
-public class CorsFilter implements Filter {
+public class CORSFilter implements Filter {
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        // Filter initialization code if needed
+        // 필터 초기화 코드
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
-        HttpServletResponse res = (HttpServletResponse) response;
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        res.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with");
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
 
-        if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) request).getMethod())) {
-            res.setStatus(HttpServletResponse.SC_OK);
+        // CORS 설정
+        httpResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, X-Requested-With, remember-me");
+        httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        httpResponse.setHeader("Access-Control-Max-Age", "3600");
+
+        // Pre-flight 요청에 대한 처리
+        if ("OPTIONS".equalsIgnoreCase(httpRequest.getMethod())) {
+            httpResponse.setStatus(HttpServletResponse.SC_OK);
         } else {
-            chain.doFilter(request, response);
+            filterChain.doFilter(servletRequest, servletResponse);
         }
     }
 
     @Override
     public void destroy() {
-        // Cleanup code if needed
+        // 필터 종료 코드
     }
 }
--- src/main/java/egovframework/ateli9r/cmmn/config/WebConfig.java
+++
@@ -1,9 +0,0 @@
-package egovframework.ateli9r.cmmn.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    // No additional configuration needed for filter
-}
